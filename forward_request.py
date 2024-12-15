95a69a37e597c87a464234e0f50269909f05ca02
sahitinaidu <sahitivss@gmail.com>
2024-12-15 00:44:46 -0500

    made changes for load balancing and added dockerfiles

--- forward_request.py
+++ forward_request.py
@@ -1,18 +1,34 @@
 import random
 import requests
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+import threading
 
 app = Flask(__name__)
+CORS(app)
 
 SERVER_CONTAINERS = [
     'http://172.18.0.2:50001',
-    'http://172.18.0.5:50001',
     'http://172.18.0.3:50001',
-#    'http://172.18.0.6:50001',
 ]
 
-def get_random_server():
-    return random.choice(SERVER_CONTAINERS)
+server_connections = {server: 0 for server in SERVER_CONTAINERS}
+lock = threading.Lock()
+
+def get_least_connected_server():
+    with lock:
+        print("Current server connections:", server_connections)
+        min_connections = min(server_connections.values())
+        least_connected_servers = [server for server, count in server_connections.items() if count == min_connections]
+        print("least connected servers",least_connected_servers)
+        return random.choice(least_connected_servers)
+
+def update_server_connections(server, increment=True):
+    with lock:
+        if increment:
+            server_connections[server] += 1
+        else:
+            server_connections[server] -= 1
 
 @app.route('/search/<column>', methods=['GET'])
 def search(column):
@@ -20,14 +36,17 @@ def search(column):
     match_type = request.args.get('match_type', default='partial')
     if not query_value:
         return jsonify({"error": "Search value is required"}), 400
-
-    server_url = get_random_server()
+    server_url = get_least_connected_server()
     print(f"Forwarding request to server: {server_url}")
+    update_server_connections(server_url, increment=True)
     try:
         response = requests.get(f"{server_url}/search/{column}", params=request.args)
+        update_server_connections(server_url, increment=False)
         return jsonify(response.json()), response.status_code
     except Exception as e:
+        update_server_connections(server_url, increment=False)
         return jsonify({"error": f"Error forwarding request to server: {str(e)}"}), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=50001, debug=True)
+
